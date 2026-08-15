9e489b21d36d77691c847b3334799a0a532ebd2f
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-08-15 19:27:10 +0530

    Server

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express =  require('express');
 const app = express();
 
+
+app.get("/test",(req,res) =>{
+    res.send("Hello my loveeeee");
+});
 app.use((req,res) =>{
     res.send("Hello Monika from the server");
 })
+
 app.listen(3000,() =>{
     console.log("Server is successfully listening on port 3000...");
 } );
