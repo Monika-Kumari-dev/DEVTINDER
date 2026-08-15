d108f8de67cb2ef88a6d50c02801271b1aa68331
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-08-15 17:01:10 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express =  require('express');
+const app = express();
+
+app.use((req,res) =>{
+    res.send("Hello Monika from the server");
+})
+app.listen(3000,() =>{
+    console.log("Server is successfully listening on port 3000...");
+} );
