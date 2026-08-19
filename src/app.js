b160195bb6dd9dac632e4f7cf26ee81a138b4e6e
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-08-19 19:07:37 +0530

    Schema creation

--- src/app.js
+++ src/app.js
@@ -1,46 +1,6 @@
-const express =  require('express');
+const express = require("express");
+require("./config/database")
 const app = express();
-const {adminAuth} = require("./middlewares/auth")
-// app.delete("/user",(req,res) =>{
-//     res.send("Deleted successfully!");
-// });
-app.get("/test",(req,res) =>{
-    res.send("Hello my loveeeee");
+app.listen(1515,() =>{
+    console.log("Server is successfully listening on port 1515...");
 });
-app.use("/admin",adminAuth);
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All data sent");
-});
-
-// app.post("/user",(req,res) =>{
-//     //saving data to DB
-//     res.send("Data successfully saved to the database!");
-// })
-app.get(/^\/a(bc)?d$/,(req,res) =>{
-    res.send({firstName: "Monika",lastName:"Singh"});
-});
-// app.get("/user",(req,res)=>{
-//     console.log(req.query);
-//     res.send({firstName:"Monika",lastName:"S"});
-// });
-// app.get("/user/:userId",(req,res)=>{
-//     console.log(req.params);
-//     res.send({firstName:"Monika",lastName:"S"});
-// });
-// app.use(
-//     "/user",(req,res,next) =>{
-//         console.log("Handling the route user!!");
-//         next();
-//         res.send("Response!!");
-//     },
-//     (req,res) =>{
-//         console.log("Handling the route user 2!!");
-//         res.send("2nd Response!!");
-//     }
-// );
-
-app.listen(3000,() =>{
-    console.log("Server is successfully listening on port 3000...");
-} );
