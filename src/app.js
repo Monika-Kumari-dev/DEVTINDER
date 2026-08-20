9aa3c08caa2b8bc2228cd4b2abc98e17725fa7ce
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-08-20 11:25:46 +0530

    mongoose cluster

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
-require("./config/database")
+
+const connectDB = require("./config/database")
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup",async(req,res) =>{
+    const user = new User ({
+      firstName:"Monika",
+      lastName:"Singh",
+      emailId:"monikapratima7870@gmail.com",
+      password:"monika123@",
+    
+});
+await user.save();
+res.send("user created successfully");
+});
+connectDB();
 app.listen(1515,() =>{
     console.log("Server is successfully listening on port 1515...");
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/DEVTINDER")
-    connectDB().then(() => {
-        console.log("MongoDB connected successfully");
-    })
-    .catch((err) => {
-        console.log("MongoDB connection error:", err);
-    });
+const connectDB = async () => {
+    await mongoose.connect("mongodb://localhost:27017/DEVTINDER");
+
+    console.log("MongoDB connected successfully");
+};
+
+module.exports = connectDB;
