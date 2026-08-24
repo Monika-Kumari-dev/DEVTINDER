bd46f1412da12e4725d8a779567ea24e4e4cee09
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-08-24 14:53:23 +0530

    JWT cookies

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,39 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 const adminAuth = (req, res, next) => {
-    console.log("Admin auth is getting checked");
+  console.log("Admin auth is getting checked");
 
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
 
-    if (isAdminAuthorized) {
-        next();
-    } else {
-        res.status(401).send("Unauthorized request");
+  if (isAdminAuthorized) {
+    next();
+  } else {
+    res.status(401).send("Unauthorized request");
+  }
+};
+const userAuth = async (req, res, next) => {
+  //Read the token from the req cookies
+
+  try {
+    const { token } = req.cookies;
+    if(!token){
+        throw new Error("Token is not valid!!!!!!!");
     }
+    // Validate the token
+    const decodeObj = await jwt.verify(token, "");
+    const { _id } = decodeObj;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
 };
 
 module.exports = {
-    adminAuth
+  adminAuth,
+  userAuth,
 };
