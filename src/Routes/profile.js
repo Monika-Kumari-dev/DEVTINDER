26f19adc3b8d646d51153b7d7a4991d371c35fbd
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-08-25 19:39:32 +0530

    Patch

--- src/Routes/profile.js
+++ src/Routes/profile.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const{validateEditProfileData} = require("../utils/validation");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 profileRouter.get("/profile",userAuth,async(req,res) =>{try{
     const cookies = req.cookies;
     const {token} = cookies;
@@ -34,9 +37,19 @@ profileRouter.get("/profile/view",userAuth,async(req,res) =>{
 profileRouter.patch("/profile/edit",userAuth,async (req,res) =>{
 try{
     
-if(!validateEditProfileData(req));{
+if(!validateEditProfileData(req)){
     throw new Error("Invalid Edit Request");
 }
+const loggedInUser = req.user;
+console.log(loggedInUser);
+Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+await loggedInUser.save();
+
+res.json({
+    message: `${loggedInUser.firstName},your profile updated successfully`,
+    data:loggedInUser,
+});
+
 }catch (err) {
     res.status(400).send("ERROR :" + err.message);
 }
