0b0c067d7a399fa897029ce5fda708f70e00d25c
Monika Kumari <your-github-monikapratima7870@gmail.com>
2026-09-03 18:33:16 +0530

    cors

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -4,9 +4,7 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/ConnectionRequest");
 
-const USER_SAFE_DATA =
-  "firstName lastName photoUrl age gender about skills";
-
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
 // Get all pending connection requests
 userRouter.get("/user/requests", userAuth, async (req, res) => {
@@ -16,18 +14,18 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId",USER_SAFE_DATA);
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
     res.json({
       message: "Data fetched successfully",
       data: connectionRequests,
     });
-
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
   }
 });
-
 
 // Get all connections
 userRouter.get("/user/connections", userAuth, async (req, res) => {
@@ -60,13 +58,38 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({
       data,
     });
-
   } catch (err) {
     res.status(400).send({
       message: err.message,
     });
   }
 });
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
 
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    console.log(hideUsersFromFeed);
+    res.send(connectionRequests);
+
+  const users = await User.find({
+    $and:[
+      {
+        _id:{ $nin: Array.from(hiddenUsersFromfeed)} 
+      },
+      { _id: {$ne: loggedInUser._id }},
+    ],
+  });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 module.exports = userRouter;
--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
-const connectDB = require("./config/database")
+app.use(cors());
+const connectDB = require("./config/database");
 const jwt = require("jsonwebtoken");
+
+
+
+
 const authRouter = require("./Routes/auth");
 const profileRouter = require("./Routes/profile");
 const RequestRouter = require("./Routes/Request");
@@ -11,99 +17,97 @@ const userRouter = require("./Routes/user");
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 // const jwt = require("jsonwebtoken");
-app.use("/",authRouter);
-app.use("/",RequestRouter);
-app.use("/",profileRouter);
+app.use("/", authRouter);
+app.use("/", RequestRouter);
+app.use("/", profileRouter);
 app.use("/", userRouter);
 
-
-app.get("/user",async(req,res) =>{
-    const userEmail = req.query.emailId;
-    try{
-        const users = await User.find({emailId:userEmail});
-        if(users.length === 0){
-            res.status(404).send("User not found");
-    } else{
-        res.send(users);
-    }}catch (err){
-        res.status(400).send("Something went wrong");
+app.get("/user", async (req, res) => {
+  const userEmail = req.query.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
-//Feed API -GET /feed - get all the users from the database 
-app.get("/feed",(req,res) =>{});
+//Feed API -GET /feed - get all the users from the database
+app.get("/feed", (req, res) => {});
 
-app.delete("/user",async(req,res) =>{
-    const userId = req.body.userId;
-    try{
-        const user = await User.findByIdAndDelete({_id: userId});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
 
-
-        res.send("User deleted successfully");
-    }catch(err){
-        res.status(400).send("Something went wrong");
-    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 //Update data of the user
-app.patch("/user",async(req,res) =>{
-    const userId = req.body.userId;
-    const data = req.body;
-    try{
-        const user = await User.findByIdAndUpdate({_id: userId },data,{
-            returnDocument: "after",
-        });
-        console.log(user);
-        res.send("User updated successfully");
-    } catch (err){
-        res.status(400).send("UPDATE FAILED:" + err.message);
-    }
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:" + err.message);
+  }
 });
-app.patch("/user/:userId",async(req,res)=>{
-    const userId = req.params?.userId;
-    const data = req.body;
-    try{
-        const ALLOWED_UPDATES = ["about","gender","age","Skills"];
-        const isUpdateAllowed = Object.keys(data).every((K)=>
-            ALLOWED_UPDATES.includes(K)
-        );
-        if(!isUpdateAllowed){
-            throw new Error("Update not allowed");
-
-        }if(data?.Skills?.length > 10){
-            throw new Error("Skills cannot be more than 10");
-        }const user = await User.findByIdAndUpdate({_id: userId},data,{
-            returnDocument: "after",
-            runValidators: true,
-        });
-        res.send(user);
-       
-    }catch(err){
-        res.status(400).send("Update Failed"+err.message);
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATES = ["about", "gender", "age", "Skills"];
+    const isUpdateAllowed = Object.keys(data).every((K) =>
+      ALLOWED_UPDATES.includes(K),
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
     }
+    if (data?.Skills?.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Update Failed" + err.message);
+  }
 });
-app.post("/login",async(req,res) =>{
-    try{
-        const {emailId,password } = req.body;
-        const user = await User.findOne({emailId: emailId});
-        if(!user){
-            throw new Error("Invalid Credentials");
-        }
-        const isPasswordValid = await user.validatePassword(password);
-    if(isPasswordValid){
-        //Create a JWT token 
-        const token = await jwt.sign({_id:user._id},"Monikatinder$123");
-        console.log(token);
-        res.cookie("token",token);
-        res.send("Login Successfully!!!");
-    }else{
-        throw new Error(" Invalid credential");
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
     }
-    }catch (err){
-        res.status(400).send("Error: "+err.message);
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      //Create a JWT token
+      const token = await jwt.sign({ _id: user._id }, "Monikatinder$123");
+      console.log(token);
+      res.cookie("token", token);
+      res.send("Login Successfully!!!");
+    } else {
+      throw new Error(" Invalid credential");
     }
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
 });
-
 
 connectDB();
-app.listen(1515,() =>{
-    console.log("Server is successfully listening on port 1515...");
+app.listen(1515, () => {
+  console.log("Server is successfully listening on port 1515...");
 });
